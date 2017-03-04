aedecd339f83982cc4cc1dbfd0871aa9d0f6c82b
Gabriel Tapizquent <gaboth22@gmail.com>
2017-03-03 19:35:45 -0500

    First Commit

---
+++ Test/FirstTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by gaboth22 on 3/3/17.
+ */
+
+public class FirstTest {
+
+    @Test
+    public void HelloWorldTest() {
+        Assert.assertEquals("Hello World!", "Hello World!");
+    }
+}
