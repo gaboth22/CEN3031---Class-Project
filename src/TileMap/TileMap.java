8a6f6f296c39680a2ac338a6cb5a82c135ba8f0d
Alexander Rumak <alexanderrumak@gmail.com>
2017-03-19 04:38:08 -0400

    Added Hexagon to represent a hexagon on the board. A hexagon would be a top level projection of the hexagons on the board, Meaning: If you have 6 hexagons on top of eachother, the TileMap class would only return the top Hexagon. Also changed TileMap to an interface

--- src/TileMap/TileMap.java
+++ src/TileMap/TileMap.java
@@ -1,4 +1,19 @@
 package TileMap;
 
-public class TileMap {
+import Terrain.TerrainLocation.TerrainLocation;
+import Tile.Tile.Tile;
+
+import java.util.List;
+
+public interface TileMap {
+
+    void insertTile(Tile tile) throws LocationOccupiedException;
+    Hexagon getHexagonAt(TerrainLocation location);
+
+    /*
+     * Returns a projection of all the hexagons at the top of each location in the map.
+     * This could be used for meepleMap validation, for gui printing purposes, etc.
+     */
+    List<Hexagon> getAllTopHexagons();
+
 }
