952ac2f22be2e9d98b8212e7d0695899daeda6ee
Alexander Rumak <alexanderrumak@gmail.com>
2017-03-19 02:39:09 -0400

    Part 1 of the implementation of Axial Movement as a class. Also, renamed TimeImpl to TileImpl (This also modified the TileFactory

---
+++ Test/TerrainTest/AxialMovementTest.java
@@ -0,0 +1,15 @@
+package TerrainTest;
+
+import Terrain.TerrainLocation.TerrainLocation;
+import org.junit.Test;
+
+public class AxialMovementTest {
+
+    TerrainLocation currentLocation;
+    TerrainLocation expectedLocation;
+
+    @Test
+    void getLocationAboveTerrain(){
+
+    }
+}
