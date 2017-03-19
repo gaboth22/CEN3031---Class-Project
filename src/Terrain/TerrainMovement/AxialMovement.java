aaf6207889cc56da82a1d78b9717afc03c2b7117
Alexander Rumak <alexanderrumak@gmail.com>
2017-03-19 03:30:47 -0400

    Unit tests and implementation for Terrain Movement

--- src/Terrain/TerrainMovement/AxialMovement.java
+++ src/Terrain/TerrainMovement/AxialMovement.java
@@ -11,26 +11,26 @@ public class AxialMovement implements TerrainMovement {
 
     @Override
     public TerrainLocation upRight(TerrainLocation loc) {
-        return null;
+        return new TerrainLocation(loc.getRow(), loc.getCol() + 1);
     }
 
     @Override
     public TerrainLocation downRight(TerrainLocation loc) {
-        return null;
+        return new TerrainLocation(loc.getRow() - 1, loc.getCol() + 1);
     }
 
     @Override
     public TerrainLocation down(TerrainLocation loc) {
-        return null;
+        return new TerrainLocation(loc.getRow() - 1, loc.getCol());
     }
 
     @Override
     public TerrainLocation downLeft(TerrainLocation loc) {
-        return null;
+        return new TerrainLocation(loc.getRow(), loc.getCol() - 1);
     }
 
     @Override
     public TerrainLocation upLeft(TerrainLocation loc) {
-        return null;
+        return new TerrainLocation(loc.getRow() + 1, loc.getCol() - 1);
     }
 }
