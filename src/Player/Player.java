a9a558c97ba971e23a8a9a2c8ee03fccdfaa1155
Peter2036 <peterlrose3@gmail.com>
2017-03-22 21:36:02 -0400

    Tiger piece type

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -3,14 +3,17 @@ package Player;
 public class Player {
     private static final int MAX_MEEPLE_COUNT = 20;
     private static final int MAX_TOTORO_COUNT = 3;
+    private static final int MAX_TIGER_COUNT = 2;
     private PlayerID id;
     private int meeples;
     private int totoros;
+    private int tigers;
 
     public Player(PlayerID id) {
         this.id = id;
         this.meeples = new Integer(MAX_MEEPLE_COUNT);
         this.totoros= new Integer(MAX_TOTORO_COUNT);
+        this.tigers = new Integer(MAX_TIGER_COUNT);
     }
 
     public PlayerID getID() {
@@ -29,6 +32,15 @@ public class Player {
     public int getTotoro() {
         if(this.totoros > 0) {
             this.totoros--;
+            return 1;
+        }
+
+        return -1;
+    }
+
+    public int getTiger() {
+        if (this.tigers > 0) {
+            this.tigers--;
             return 1;
         }
 
