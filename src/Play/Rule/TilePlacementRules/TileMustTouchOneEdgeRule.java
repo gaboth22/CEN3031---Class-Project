c9ec4e6bf3d0cb4619335f4c720702caf4396f1f
Gabriel Tapizquent <gaboth22@gmail.com>
2017-03-23 00:24:56 -0400

    TileMustTouchOneEdgeRule

--- src/Play/Rule/TilePlacementRules/TileMustTouchOneEdgeRule.java
+++ src/Play/Rule/TilePlacementRules/TileMustTouchOneEdgeRule.java
@@ -2,6 +2,7 @@ package Play.Rule.TilePlacementRules;
 
 import GamePieceMap.GamePieceMap;
 import Play.Rule.PlacementRuleException.InvalidTilePlacementRuleException;
+import Location.Location;
 import Tile.Tile.Tile;
 import TileMap.TileMap;
 
@@ -9,5 +10,10 @@ public class TileMustTouchOneEdgeRule {
 
     public static void applyRule(TileMap tileMap, GamePieceMap gamePieceMap, Tile tileToPlace)
             throws InvalidTilePlacementRuleException {
+
+        Location[] locations = tileToPlace.getArrayOfTerrainLocations();
+        for(int i = 0; i < locations.length; i++) {
+
+        }
     }
 }
