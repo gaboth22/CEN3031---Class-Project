668d0e431c3044749f268acbc85a79b6211d160f
Peter2036 <peterlrose3@gmail.com>
2017-03-24 11:57:06 -0400

    working on rules

---
+++ Test/PieceBuilder/PieceBuilder.java
@@ -0,0 +1,7 @@
+package PieceBuilder;
+
+/**
+ * Created by peter on 3/23/17.
+ */
+public class PieceBuilder {
+}
---
+++ Test/PieceBuilder/PieceBuilderTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by peter on 3/23/17.
+ */
+public class PieceBuilderTest {
+
+}
