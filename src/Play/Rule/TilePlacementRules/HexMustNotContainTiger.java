90d914be64a455c9649bd4a1dbeb4df0fc09c869
farinak <Farina@Farinas-MacBook-Pro.local>
2017-03-24 15:41:59 -0400

    Fixed issues with new rules

--- src/Play/Rule/TilePlacementRules/HexMustNotContainTiger.java
+++ src/Play/Rule/TilePlacementRules/HexMustNotContainTiger.java
@@ -2,16 +2,16 @@ package Play.Rule.TilePlacementRules;
 
 import Tile.Tile.Tile;
 import Location.Location;
-import GameMap.*;
+import GamePieceMap.*;
 import Play.Rule.PlacementRuleException.InvalidTilePlacementRuleException;
 
 public class HexMustNotContainTiger {
     privateStaticString errorMessage = "You cannot place a tile on a tiger playground.";
 
-    public static void applyRule(GameMap pieceMap, TileMap tileMap, Tile tileToPlace)
+    public static void applyRule(GameMap pieceMap, Tile tileToPlace)
             throws InvalidTilePlacementRuleException {
         Location[] locations =  tileToPlace.getArrayOfTileLocations();
-        for (int i = 0; i < locations.length(); i++) {
+        for (int i = 0; i < 3; i++) {
             if (!pieceMap.containsKey(locations[i])) {
                 break;
             }
--- src/Play/Rule/TilePlacementRules/HexMustNotContainTotoro.java
+++ src/Play/Rule/TilePlacementRules/HexMustNotContainTotoro.java
@@ -2,16 +2,16 @@ package Play.Rule.TilePlacementRules;
 
 import Tile.Tile.Tile;
 import Location.Location;
-import GameMap.*;
+import GamePieceMap.*;
 import Play.Rule.PlacementRuleException.InvalidTilePlacementRuleException;
 
 public class HexMustNotContainTotoro {
     privateStaticString errorMessage = "You cannot place a tile on a totoro sanctuary.";
 
-    public static void applyRule(GameMap pieceMap, TileMap tileMap, Tile tileToPlace)
+    public static void applyRule(GameMap pieceMap, Tile tileToPlace)
         throws InvalidTilePlacementRuleException {
         Location[] locations =  tileToPlace.getArrayOfTileLocations();
-        for (int i = 0; i < locations.length(); i++) {
+        for (int i = 0; i < 3; i++) {
             if (!pieceMap.containsKey(locations[i])) {
                 break;
             }
