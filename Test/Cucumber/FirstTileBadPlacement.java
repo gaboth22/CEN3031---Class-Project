c588e01d73f550b3cfc4ced05dd3dbb99391578e
Gabriel Tapizquent <gaboth22@gmail.com>
2017-03-24 15:47:50 -0400

    negative

---
+++ Test/Cucumber/FirstTileBadPlacement.java
@@ -0,0 +1,54 @@
+package Cucumber;
+
+import GameBoard.*;
+import Location.Location;
+import Movement.*;
+import Play.TilePlacementPhase.TilePlacementPhase;
+import Player.PlayerID;
+import TileBuilder.TileBuilder;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import Tile.Tile.Tile;
+import org.junit.Assert;
+
+public class FirstTileBadPlacement {
+    private GameBoard gameBoard;
+    private TileBuilder builder;
+    private TilePlacementPhase placementPhase;
+    private Location origin;
+    private Tile firstTile;
+    private Movement movement;
+    private boolean insertionFailed;
+
+    @Given("^it is the first turn$")
+    public void InitializeGameboardAgain() {
+        insertionFailed = false;
+        builder = new TileBuilder();
+        origin = new Location(0, 0);
+        gameBoard = new GameBoardImpl();
+        movement = new AxialMovement();
+        Assert.assertEquals(1, gameBoard.getCurrentTurn());
+    }
+
+    @When("^the active player places a tile not at the origin$")
+    public void placeFirstTileAgain() throws Exception {
+        Location notOrigin = new Location(1,2);
+        firstTile = builder.getTileWithLocations(notOrigin,
+                                                movement.up(notOrigin),
+                                                movement.upRight(notOrigin));
+        placementPhase = new TilePlacementPhase(PlayerID.PLAYER_ONE, firstTile);
+
+        try {
+            gameBoard.doTilePlacementPhase(placementPhase);
+        }
+        catch(Exception e) {
+            insertionFailed = true;
+        }
+    }
+
+    @Then("^then the insertion should not be valid$")
+    public void theInsertionShouldBeInvalid() {
+        Assert.assertTrue(insertionFailed);
+    }
+}
