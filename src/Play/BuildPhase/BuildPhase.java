b3a39aaa1877e32e61527ce11653f11c684017c8
Gabriel Tapizquent <gaboth22@gmail.com>
2017-03-25 01:29:44 -0400

    Added BuildPhase class

--- src/Play/BuildPhase/BuildPhase.java
+++ src/Play/BuildPhase/BuildPhase.java
@@ -1,4 +1,30 @@
 package Play.BuildPhase;
 
+import GamePieceMap.TypeOfPiece;
+import Player.PlayerID;
+import Location.Location;
+
 public class BuildPhase {
+    final private PlayerID playerID;
+    final private TypeOfPiece pieceToPlace;
+    final private Location locationToPlacePieceAt;
+
+    public BuildPhase(PlayerID playerID, TypeOfPiece pieceToPlace, Location locationToPlacePieceAt) {
+        this.playerID = playerID;
+        this.pieceToPlace = pieceToPlace;
+        this.locationToPlacePieceAt = locationToPlacePieceAt;
+    }
+
+    public PlayerID getPlayerID() {
+        return playerID;
+    }
+
+    public TypeOfPiece getPieceToPlace() {
+        return pieceToPlace;
+    }
+
+    public Location getLocationToPlacePieceAt() {
+        return locationToPlacePieceAt;
+    }
+
 }
