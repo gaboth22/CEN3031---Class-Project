a6195d01583b20feb5c4b2273c953bcda8e913e6
Gabriel Tapizquent <gaboth22@gmail.com>
2017-03-25 00:47:39 -0400

    Restructured and refactor PlayerMustHavePieceRule

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,46 +1,46 @@
 package Player;
 
 public class Player {
-    private static final int MAX_MEEPLE_COUNT = 20;
+    private static final int MAX_VILLAGER_COUNT = 20;
     private static final int MAX_TOTORO_COUNT = 3;
     private static final int MAX_TIGER_COUNT = 2;
     private PlayerID id;
-    private int meeples;
-    private int totoros;
-    private int tigers;
+    private int villagerCount;
+    private int totoroCount;
+    private int tigerCount;
 
     public Player(PlayerID id) {
         this.id = id;
-        this.meeples = new Integer(MAX_MEEPLE_COUNT);
-        this.totoros= new Integer(MAX_TOTORO_COUNT);
-        this.tigers = new Integer(MAX_TIGER_COUNT);
+        this.villagerCount = new Integer(MAX_VILLAGER_COUNT);
+        this.totoroCount = new Integer(MAX_TOTORO_COUNT);
+        this.tigerCount = new Integer(MAX_TIGER_COUNT);
     }
 
     public PlayerID getID() {
         return this.id;
     }
 
-    public void decrimentMeeples() {
-        this.meeples--;
+    public void decrementVillagerCount() {
+        this.villagerCount--;
     }
 
-    public void decrimentTotoros() {
-            this.totoros--;
+    public void decrementTotoroCount() {
+            this.totoroCount--;
     }
 
-    public void decrimentTigers() {
-            this.tigers--;
+    public void decrementTigerCount() {
+            this.tigerCount--;
     }
 
-    public int getMeeples(){
-        return meeples;
+    public int getVillagerCount(){
+        return villagerCount;
     }
 
-    public int getTotoros(){
-        return totoros;
+    public int getTotoroCount(){
+        return totoroCount;
     }
 
-    public int getTigers(){
-        return tigers;
+    public int getTigerCount(){
+        return tigerCount;
     }
 }
