499879772f6fd6d1707815de448fd8d566b57907
Peter2036 <peterlrose3@gmail.com>
2017-03-27 15:43:54 -0400

    phase type getters and setters

--- src/Play/TilePlacementPhase/TilePlacementPhase.java
+++ src/Play/TilePlacementPhase/TilePlacementPhase.java
@@ -6,10 +6,23 @@ import Tile.Tile.Tile;
 public class TilePlacementPhase {
     final private PlayerID playerID;
     final private Tile tileToPlace;
+    private TilePlacementType type;
 
     public TilePlacementPhase(PlayerID playerID, Tile tileToPlace) {
         this.playerID = playerID;
         this.tileToPlace = tileToPlace;
+        this.type = null;
+    }
+
+    public void setTilePlacementType(TilePlacementType type){
+        this.type = type;
+    }
+
+    public TilePlacementType getTilePlacementType(){
+        if(type == null){
+            throw new NullPointerException("Trying to access TilePlacementType without setting value");
+        }
+        return type;
     }
 
     public PlayerID getPlayerID() {
