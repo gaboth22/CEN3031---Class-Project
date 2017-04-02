d97c6033f03012df5e722eef8e68ec3786bfddd0
Peter2036 <peterlrose3@gmail.com>
2017-04-02 00:50:59 -0400

    work on gameboardimpl test

--- Test/GameBoardTest/GameBoardImplTest.java
+++ Test/GameBoardTest/GameBoardImplTest.java
@@ -13,6 +13,7 @@ import Player.PlayerID;
 import Settlements.Creation.Settlement;
 import Tile.Tile.TileImpl;
 import TileMap.Hexagon;
+import cucumber.api.java.gl.E;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -130,6 +131,8 @@ public class GameBoardImplTest {
 
         GamePieceMap placedGamePieces = gameBoard.getGamePieceMap();
         checkSettlementSuccessfullyFounded(placedGamePieces, buildPhase);
+
+        assertEquals(1, gameBoard.getPlayerOneScore());
     }
 
     private void checkSettlementSuccessfullyFounded(GamePieceMap placedGamePieces, BuildPhase buildPhase) {
@@ -187,24 +190,102 @@ public class GameBoardImplTest {
         gameBoard.doBuildPhase(buildPhaseTwo);
     }
 
+    @Test
     public void TotoroPlacementSuccessTest() throws Exception{
+        GameBoard gameBoard = getSuccessfulTotoroGameBoard();
+
+        GamePiece totoroToPlace = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.TOTORO);
+        BuildPhase buildPhaseTotoro = new BuildPhase(totoroToPlace, new Location(0,-2));
+        buildPhaseTotoro.setBuildType(BuildType.PLACE_TOTORO);
+        gameBoard.doBuildPhase(buildPhaseTotoro);
+
+        assertEquals(205, gameBoard.getPlayerOneScore());
+    }
+
+    private GameBoard getSuccessfulTotoroGameBoard() throws Exception {
         GameBoard gameBoard = new GameBoardImpl();
 
         TilePlacementPhase tilePlacementPhaseOne = new TilePlacementPhase(PlayerID.PLAYER_ONE, getSimpleTileToPlace());
         tilePlacementPhaseOne.setTilePlacementType(TilePlacementType.SIMPLE_PLACEMENT);
         gameBoard.doTilePlacementPhase(tilePlacementPhaseOne);
 
-        TilePlacementPhase tilePlacementPhase = new TilePlacementPhase(PlayerID.PLAYER_ONE, getSecondSimpleTile());
+        TilePlacementPhase tilePlacementPhaseTwo = new TilePlacementPhase(PlayerID.PLAYER_ONE, getSecondSimpleTile());
+        tilePlacementPhaseTwo.setTilePlacementType(TilePlacementType.SIMPLE_PLACEMENT);
+        gameBoard.doTilePlacementPhase(tilePlacementPhaseTwo);
 
-        GamePiece gamePiece = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.VILLAGER);
-        BuildPhase buildPhase = new BuildPhase(gamePiece, new Location)
+        GamePiece gamePieceOne = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.VILLAGER);
+        BuildPhase buildPhaseOne = new BuildPhase(gamePieceOne, new Location(0,2));
+        buildPhaseOne.setBuildType(BuildType.FOUND);
+        gameBoard.doBuildPhase(buildPhaseOne);
+
+        GamePiece gamePieceTwo = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.VILLAGER);
+        BuildPhase buildPhaseTwo = new BuildPhase(gamePieceTwo, new Location(0,1));
+        buildPhaseTwo.setBuildType(BuildType.FOUND);
+        gameBoard.doBuildPhase(buildPhaseTwo);
+
+        GamePiece gamePieceThree = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.VILLAGER);
+        BuildPhase buildPhaseThree = new BuildPhase(gamePieceThree, new Location(1,0));
+        buildPhaseThree.setBuildType(BuildType.FOUND);
+        gameBoard.doBuildPhase(buildPhaseThree);
+
+        GamePiece gamePieceFour = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.VILLAGER);
+        BuildPhase buildPhaseFour = new BuildPhase(gamePieceFour, new Location(1,-1));
+        buildPhaseFour.setBuildType(BuildType.FOUND);
+        gameBoard.doBuildPhase(buildPhaseFour);
+
+        GamePiece gamePieceFive = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.VILLAGER);
+        BuildPhase buildPhaseFive = new BuildPhase(gamePieceFive, new Location(0,-1));
+        buildPhaseFive.setBuildType(BuildType.FOUND);
+        gameBoard.doBuildPhase(buildPhaseFive);
+
+        return gameBoard;
+    }
+
+
+    @Test(expected = BuildPhaseException.class)
+    public void TotoroPlacementFailureTest() throws Exception{
+        GameBoard gameBoard = getFailingTotoroGameBoard();
+
+        GamePiece totoroToPlace = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.TOTORO);
+        BuildPhase buildPhaseTotoro = new BuildPhase(totoroToPlace, new Location(0,-2));
+        buildPhaseTotoro.setBuildType(BuildType.PLACE_TOTORO);
+        gameBoard.doBuildPhase(buildPhaseTotoro);
+    }
+
+    private GameBoard getFailingTotoroGameBoard() throws Exception{
+        GameBoard gameBoard = new GameBoardImpl();
+
+        TilePlacementPhase tilePlacementPhaseOne = new TilePlacementPhase(PlayerID.PLAYER_ONE, getSimpleTileToPlace());
+        tilePlacementPhaseOne.setTilePlacementType(TilePlacementType.SIMPLE_PLACEMENT);
+        gameBoard.doTilePlacementPhase(tilePlacementPhaseOne);
+
+        TilePlacementPhase tilePlacementPhaseTwo = new TilePlacementPhase(PlayerID.PLAYER_ONE, getSecondSimpleTile());
+        tilePlacementPhaseTwo.setTilePlacementType(TilePlacementType.SIMPLE_PLACEMENT);
+        gameBoard.doTilePlacementPhase(tilePlacementPhaseTwo);
+
+        GamePiece gamePieceOne = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.VILLAGER);
+        BuildPhase buildPhaseOne = new BuildPhase(gamePieceOne, new Location(0,2));
+        buildPhaseOne.setBuildType(BuildType.FOUND);
+        gameBoard.doBuildPhase(buildPhaseOne);
+
+        GamePiece gamePieceTwo = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.VILLAGER);
+        BuildPhase buildPhaseTwo = new BuildPhase(gamePieceTwo, new Location(0,1));
+        buildPhaseTwo.setBuildType(BuildType.FOUND);
+        gameBoard.doBuildPhase(buildPhaseTwo);
+
+        GamePiece gamePieceThree = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.VILLAGER);
+        BuildPhase buildPhaseThree = new BuildPhase(gamePieceThree, new Location(1,0));
+        buildPhaseThree.setBuildType(BuildType.FOUND);
+        gameBoard.doBuildPhase(buildPhaseThree);
+
+        return gameBoard;
     }
 
     private Tile getSecondSimpleTile(){
         Location[] locations = new Location[] {
-                new Location(1, 1),
-                new Location(0,1),
-                new Location(0,2)
+                new Location(1, -2),
+                new Location(0,-2),
+                new Location(0,-1)
         };
 
         Terrain[] terrains = new Terrain[] {
@@ -217,12 +298,128 @@ public class GameBoardImplTest {
     }
 
     @Test
-    public void getCurrentTurn() throws Exception {
+    public void TigerPlacementSuccessTest() throws Exception{
+        GameBoard gameBoard = getSuccessfulTigerGameBoard();
 
+        GamePiece tigerToPlace = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.TIGER);
+        BuildPhase buildPhaseTiger = new BuildPhase(tigerToPlace, new Location(0,-1));
+        buildPhaseTiger.setBuildType(BuildType.PLACE_TIGER);
+        gameBoard.doBuildPhase(buildPhaseTiger);
+
+        assertEquals(76, gameBoard.getPlayerOneScore());
+    }
+
+    private GameBoard getSuccessfulTigerGameBoard() throws Exception {
+        GameBoardImpl gameBoard = new GameBoardImpl();
+
+        TilePlacementPhase tilePlacementPhaseOne = new TilePlacementPhase(PlayerID.PLAYER_ONE, getSimpleTileToPlace());
+        tilePlacementPhaseOne.setTilePlacementType(TilePlacementType.SIMPLE_PLACEMENT);
+        gameBoard.doTilePlacementPhase(tilePlacementPhaseOne);
+
+        TilePlacementPhase tilePlacementPhaseTwo = new TilePlacementPhase(PlayerID.PLAYER_ONE, generateTileForNuking());
+        tilePlacementPhaseTwo.setTilePlacementType(TilePlacementType.NUKE);
+        gameBoard.doTilePlacementPhase(tilePlacementPhaseTwo);
+
+        TilePlacementPhase tilePlacementPhaseThree = new TilePlacementPhase(PlayerID.PLAYER_ONE, getSecondSimpleTile());
+        tilePlacementPhaseThree.setTilePlacementType(TilePlacementType.SIMPLE_PLACEMENT);
+        gameBoard.doTilePlacementPhase(tilePlacementPhaseThree);
+
+        TilePlacementPhase tilePlacementPhaseFour
+                = new TilePlacementPhase(PlayerID.PLAYER_ONE, generateSecondTileForNuking());
+        tilePlacementPhaseFour.setTilePlacementType(TilePlacementType.NUKE);
+        gameBoard.doTilePlacementPhase(tilePlacementPhaseFour);
+
+        TilePlacementPhase tilePlacementPhaseFive
+                = new TilePlacementPhase(PlayerID.PLAYER_ONE, generateThirdTileForNuking());
+        tilePlacementPhaseFive.setTilePlacementType(TilePlacementType.NUKE);
+        gameBoard.doTilePlacementPhase(tilePlacementPhaseFive);
+
+        GamePiece gamePieceOne = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.VILLAGER);
+        BuildPhase buildPhaseOne = new BuildPhase(gamePieceOne, new Location(-1,0));
+        buildPhaseOne.setBuildType(BuildType.FOUND);
+        gameBoard.doBuildPhase(buildPhaseOne);
+
+        return gameBoard;
+    }
+
+    @Test(expected = BuildPhaseException.class)
+    public void TigerPlacementFailureTest() throws Exception {
+        GameBoard gameBoard = new GameBoardImpl();
+
+        GamePiece tigerToPlace = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.TOTORO);
+        BuildPhase buildPhaseTiger = new BuildPhase(tigerToPlace, new Location(1,-1));
+        buildPhaseTiger.setBuildType(BuildType.PLACE_TIGER);
+        gameBoard.doBuildPhase(buildPhaseTiger);
+    }
+
+    private Tile generateTileForNuking() {
+        Location[] locations = new Location[] {
+                new Location(0, 0),
+                new Location(0,1),
+                new Location(1,0)
+        };
+
+        Terrain[] terrains = new Terrain[] {
+                Terrain.VOLCANO,
+                Terrain.GRASSLANDS,
+                Terrain.GRASSLANDS
+        };
+
+        return new TileImpl(Arrays.asList(terrains), Arrays.asList(locations));
+    }
+
+    private Tile generateSecondTileForNuking(){
+        Location[] locations = new Location[] {
+                new Location(1, -2),
+                new Location(0,-1),
+                new Location(1,-1)
+        };
+
+        Terrain[] terrains = new Terrain[] {
+                Terrain.VOLCANO,
+                Terrain.GRASSLANDS,
+                Terrain.GRASSLANDS
+        };
+
+        return new TileImpl(Arrays.asList(terrains), Arrays.asList(locations));
+    }
+
+    private Tile generateThirdTileForNuking(){
+        Location[] locations = new Location[] {
+                new Location(0, 0),
+                new Location(1,-1),
+                new Location(0,-1)
+        };
+
+        Terrain[] terrains = new Terrain[] {
+                Terrain.VOLCANO,
+                Terrain.GRASSLANDS,
+                Terrain.GRASSLANDS
+        };
+
+        return new TileImpl(Arrays.asList(terrains), Arrays.asList(locations));
     }
 
     @Test
-    public void getPlacedHexagons() throws Exception {
+    public void CurrentTurnTest() throws Exception {
+        GameBoard gameBoard = new GameBoardImpl();
+
+        TilePlacementPhase tilePlacementPhaseOne = new TilePlacementPhase(PlayerID.PLAYER_ONE, getSimpleTileToPlace());
+        tilePlacementPhaseOne.setTilePlacementType(TilePlacementType.SIMPLE_PLACEMENT);
+        gameBoard.doTilePlacementPhase(tilePlacementPhaseOne);
+
+        assertEquals(1, gameBoard.getCurrentTurn());
+
+        GamePiece gamePieceOne = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.VILLAGER);
+        BuildPhase buildPhaseOne = new BuildPhase(gamePieceOne, new Location(-1,0));
+        buildPhaseOne.setBuildType(BuildType.FOUND);
+        gameBoard.doBuildPhase(buildPhaseOne);
+
+        assertEquals(2, gameBoard.getCurrentTurn());
+    }
+
+    @Test
+    public void getPlacedHexagonsTest() throws Exception {
 
     }
 
@@ -232,36 +429,72 @@ public class GameBoardImplTest {
     }
 
     @Test
-    public void getPlayer() throws Exception {
-
+    public void getPlayerScoreTest() throws Exception {
+        GameBoard gameBoard = new GameBoardImpl();
+        assertEquals(0, gameBoard.getPlayerOneScore());
+        assertEquals(0, gameBoard.getPlayerTwoScore());
     }
 
     @Test
-    public void getPlayerOneScore() throws Exception {
-    }
+    public void getPlayerSettlementsTest() throws Exception {
+        GameBoard gameBoard = new GameBoardImpl();
+        List<Settlement> playerOneSettlements = gameBoard.getPlayerOneSettlements();
+        List<Settlement> playerTwoSettlements = gameBoard.getPlayerOneSettlements();
 
-    @Test
-    public void getPlayerTwoScore() throws Exception {
+        assertEquals(0, playerOneSettlements.size());
+        assertEquals(0, playerTwoSettlements.size());
 
-    }
+        GamePiece gamePieceOne = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.VILLAGER);
+        BuildPhase buildPhaseOne = new BuildPhase(gamePieceOne, new Location(1,0));
+        buildPhaseOne.setBuildType(BuildType.FOUND);
+        gameBoard.doBuildPhase(buildPhaseOne);
 
-    @Test
-    public void getPlayerOneSettlements() throws Exception {
+        playerOneSettlements = gameBoard.getPlayerOneSettlements();
+        playerTwoSettlements = gameBoard.getPlayerTwoSettlements();
 
-    }
+        assertEquals(1, playerOneSettlements.size());
+        assertEquals(0, playerTwoSettlements.size());
 
-    @Test
-    public void getPlayerTwoSettlements() throws Exception {
+        TilePlacementPhase tilePlacementPhaseOne = new TilePlacementPhase(PlayerID.PLAYER_ONE, getSimpleTileToPlace());
+        tilePlacementPhaseOne.setTilePlacementType(TilePlacementType.SIMPLE_PLACEMENT);
+        gameBoard.doTilePlacementPhase(tilePlacementPhaseOne);
 
+        GamePiece gamePieceTwo = new GamePiece(PlayerID.PLAYER_ONE, TypeOfPiece.VILLAGER);
+        BuildPhase buildPhaseTwo = new BuildPhase(gamePieceTwo, new Location(0,1));
+        buildPhaseTwo.setBuildType(BuildType.FOUND);
+        gameBoard.doBuildPhase(buildPhaseTwo);
+
+        playerOneSettlements = gameBoard.getPlayerOneSettlements();
+        playerTwoSettlements = gameBoard.getPlayerTwoSettlements();
+
+        assertEquals(1, playerOneSettlements.size());
+        assertEquals(0, playerTwoSettlements.size());
+
+        GamePiece gamePieceThree = new GamePiece(PlayerID.PLAYER_TWO, TypeOfPiece.VILLAGER);
+        BuildPhase buildPhaseThree = new BuildPhase(gamePieceThree, new Location(0,2));
+        buildPhaseThree.setBuildType(BuildType.FOUND);
+        gameBoard.doBuildPhase(buildPhaseThree);
+
+        playerOneSettlements = gameBoard.getPlayerOneSettlements();
+        playerTwoSettlements = gameBoard.getPlayerTwoSettlements();
+
+        assertEquals(1, playerOneSettlements.size());
+        assertEquals(1, playerTwoSettlements.size());
+
+        GamePiece gamePieceFour = new GamePiece(PlayerID.PLAYER_TWO, TypeOfPiece.VILLAGER);
+        BuildPhase buildPhaseFour = new BuildPhase(gamePieceFour, new Location(-1,0));
+        buildPhaseFour.setBuildType(BuildType.FOUND);
+        gameBoard.doBuildPhase(buildPhaseFour);
+
+        playerOneSettlements = gameBoard.getPlayerOneSettlements();
+        playerTwoSettlements = gameBoard.getPlayerTwoSettlements();
+
+        assertEquals(1, playerOneSettlements.size());
+        assertEquals(2, playerTwoSettlements.size());
     }
 
     @Test
     public void getPlaceableLocations() throws Exception {
-
-    }
-
-    @Test
-    public void getGamePieceMap() throws Exception {
 
     }
 
