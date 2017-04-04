a724fcb9eaf81d8551da69979364ff56741a4bde
Gabriel Tapizquent <gaboth22@gmail.com>
2017-04-04 11:39:06 -0400

    Fix to special piece placement rule

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import GUI.GuiThread.GuiThread;
+
+public class Main {
+    public static void main(String[] args) {
+        GuiThread guiThread = new GuiThread();
+        guiThread.start();
+    }
+}
