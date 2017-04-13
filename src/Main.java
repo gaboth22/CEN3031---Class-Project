aaec8f22674464a6b94f313f299455b3bc08b29c
Gabriel Tapizquent <gaboth22@gmail.com>
2017-04-13 13:14:34 -0400

    Added tournament infor parser and commented out broken expansion

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,31 @@ import GameMaster.ServerComm.GameClient;
 import GameMaster.ServerComm.ServerClient;
 import Player.PlayerID;
 import Steve.PlayGeneration.ProfitablePlayGeneration;
-import Steve.PlayGeneration.SimplePlayGenerator;
+import TournamentInfoParser.TournamentInforParser;
 
 public class Main {
-    private static final String SERVER_IP = "10.228.1.171";
-    private static final int SERVER_PORT = 1708;
-
-    private static final String TOURNAMENT_PASSWORD = "goodluck";
-    private static final String USERNAME = "P";
-    private static final String PASSWORD = "P";
 
     private static final boolean CONSOLE_INPUT_IS_DISABLED = false;
+        private static String SERVER_IP;
+        private static int SERVER_PORT;
+        private static String TOURNAMENT_PASSWORD;
+        private static String USERNAME;
+        private static String PASSWORD;
 
     public static void main(String[] args) throws Exception {
+
+        TournamentInforParser.parse("./src/tournament.txt");
+
+        SERVER_IP = TournamentInforParser.getIpAddress();
+        SERVER_PORT = TournamentInforParser.getPortNumber();
+        TOURNAMENT_PASSWORD = TournamentInforParser.getTournamentPass();
+        USERNAME = TournamentInforParser.getUsername();
+        PASSWORD = TournamentInforParser.getPassword();
+
+
         Debug.enableAllDebugLevels();
         Debug.enableLogFile("log.log");
+
         GameClient client = new ServerClient(SERVER_IP, SERVER_PORT);
 
         Game gameOne = new Game(PlayerID.PLAYER_ONE, new ProfitablePlayGeneration());
